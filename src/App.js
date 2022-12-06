8ef54889ee8c9937c0f1bf73d8cdf33d468be7b3
mohdshahbaz <mohdshahbaz027@gmail.com>
2022-12-06 18:25:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const [temp, setTemp] = useState("");
   const [word, setWord] = useState("");
-  const [size, setSize] = useState(400);
+  const [size, setSize] = useState(200);
   const [bgColor, setBgColor] = useState("ffffff");
   const [qrCode, setQrCode] = useState("");
 
@@ -22,30 +22,32 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>QR Code Generator</h1>
-      <div className="input-box">
-        <div className="gen">
-          <input type="text" onChange={
+    <div className="container-fluid">
+      <div className="my-5 d-flex flex-column align-items-center">
+        <h1 className="text-center">QR Code Generator</h1>
+        <div id="inputText" class="mt-3 w-50 input-group">
+          <input type="text" className="form-control" onChange={
             (e) => { setTemp(e.target.value) }}
             placeholder="Enter text to encode" />
-          <button className="button"
+          <button type="button" className="btn btn-outline-warning input-group-text"
             onClick={handleClick}>
             Generate
           </button>
         </div>
-        <div className="extra">
-          <h5>Background Color:</h5>
-          <input type="color" onChange={(e) => { setBgColor(e.target.value.substring(1)) }} />
-          <h5>Dimension:</h5>
-          <input type="range" min="200" max="600"
+      </div>
+      <div className="text-center">
+        <div className="d-inline-flex">
+          <label for="color" className="form-label"><h5>Background Color:</h5></label>
+          <input type="color" id="color" className="form-control" onChange={(e) => { setBgColor(e.target.value.substring(1)) }} />
+          <label for="customRange" class="form-label ms-3"><h5>Dimension:</h5></label>
+          <input type="range" className="form-range" min="200" max="600" id="customRange"
             value={size} onChange={(e) => { setSize(e.target.value) }} />
         </div>
       </div>
-      <div className="output-box">
+      <div className="my-5 d-flex flex-column align-items-center">
         <img src={qrCode} alt="" />
         <a href={qrCode} download="QRCode">
-          <button type="button">Download</button>
+          <button type="button" className="mt-5 btn btn-outline-success">Download</button>
         </a>
       </div>
     </div>
