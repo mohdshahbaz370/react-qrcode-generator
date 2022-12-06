3ff4f73dfbc122d653da5d16fa4e9f04fcbdd83a
mohdshahbaz <mohdshahbaz027@gmail.com>
2022-12-06 20:42:24 +0530

    set header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
@@ -22,35 +23,37 @@ function App() {
   }
 
   return (
-    <div className="container-fluid">
-      <div className="my-5 d-flex flex-column align-items-center">
-        <h1 className="text-center">QR Code Generator</h1>
-        <div id="inputText" class="mt-3 w-50 input-group">
-          <input type="text" className="form-control" onChange={
-            (e) => { setTemp(e.target.value) }}
-            placeholder="Enter text to encode" />
-          <button type="button" className="btn btn-outline-warning input-group-text"
-            onClick={handleClick}>
-            Generate
-          </button>
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className=" my-5 d-flex flex-column align-items-center">
+          <div id="inputText" class="mt-3 w-50 input-group">
+            <input type="text" className="form-control" onChange={
+              (e) => { setTemp(e.target.value) }}
+              placeholder="Enter text to encode" />
+            <button type="button" className="btn btn-outline-warning input-group-text"
+              onClick={handleClick}>
+              Generate
+            </button>
+          </div>
+        </div>
+        <div className="text-center">
+          <div className="d-inline-flex">
+            <label for="color" className="form-label"><h5>Background Color:</h5></label>
+            <input type="color" id="color" className="form-control" onChange={(e) => { setBgColor(e.target.value.substring(1)) }} />
+            <label for="customRange" class="form-label ms-3"><h5>Dimension:</h5></label>
+            <input type="range" className="form-range" min="200" max="600" id="customRange"
+              value={size} onChange={(e) => { setSize(e.target.value) }} />
+          </div>
+        </div>
+        <div className="my-5 d-flex flex-column align-items-center">
+          <img src={qrCode} alt="" />
+          <a href={qrCode} download="QRCode">
+            <button type="button" className="mt-5 btn btn-outline-success">Download</button>
+          </a>
         </div>
       </div>
-      <div className="text-center">
-        <div className="d-inline-flex">
-          <label for="color" className="form-label"><h5>Background Color:</h5></label>
-          <input type="color" id="color" className="form-control" onChange={(e) => { setBgColor(e.target.value.substring(1)) }} />
-          <label for="customRange" class="form-label ms-3"><h5>Dimension:</h5></label>
-          <input type="range" className="form-range" min="200" max="600" id="customRange"
-            value={size} onChange={(e) => { setSize(e.target.value) }} />
-        </div>
-      </div>
-      <div className="my-5 d-flex flex-column align-items-center">
-        <img src={qrCode} alt="" />
-        <a href={qrCode} download="QRCode">
-          <button type="button" className="mt-5 btn btn-outline-success">Download</button>
-        </a>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,6 @@
+const Header = () => {
+
+    return (<h1 className="text-center text-bg-success p-5">QR Code Generator</h1>)
+}
+
+export default Header;
